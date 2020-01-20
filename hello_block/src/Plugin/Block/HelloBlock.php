46624aa21d610e685bd714dd22a96026b0ab320f
Carlos Amorim <carlosserraamorim@gmail.com>
2020-01-20 03:45:14 +0000

    Final

---
+++ hello_block/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ */
+namespace Drupal\hello_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Mensagem de boas-vindas personalizada
+ * @Block(
+ * id = "block_hello-block",
+ * admin_label = @Translation("hello_block"),
+ * )
+ */
+class HelloBlock extends BlockBase {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function build() {
+		$hour = date('H');
+		$dayTerm = ($hour > 19) ? "uma boa noite" : (($hour > 12) ? "uma boa tarde" : "um bom dia");;
+		return array (
+			'#type' => 'markup',
+      		'#markup' => " Tenha $dayTerm com ",
+		);
+	}
+
+}
+
+
+
